d20557165618f51a1c1de150a461370d0e3f536a
Sercurio <penalva.louis@gmail.com>
2023-10-12 11:59:44 +0200

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [sitemap(), react()],
-  site: "https://astro-theme-creek.netlify.app/"
+  site: "https://camilleprincipiano.netlify.app/",
+  markdown: {
+    // ...
+    drafts: false, // defaults to false as well but can by controlled and set to true
+  },
 });
